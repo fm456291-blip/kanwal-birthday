567288b9cf0843dcf83e46b683783216b284a2b1
fm456291-blip <fm456291@gmail.com>
2026-07-25 19:15:19 +0500

    Add files via upload

---
+++ confetti.js
@@ -0,0 +1,4 @@
+// confetti.js - Automatic Falling Confetti Animation
+const confettiSettings = { target: 'confetti-canvas' };
+const confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
